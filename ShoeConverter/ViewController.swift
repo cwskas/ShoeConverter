f3f8e1953261702a18f68ec00c07522cf22e1e09
cwskas <cwskas@gmail.com>
2015-04-02 03:18:45 -0500

    first rev

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPresed(sender: UIButton) {
+//        let sizeFromTextField = mensShoeSizeTextField.text
+//        let numberFromTextField = sizeFromTextField.toInt()
+//        var integerFromTextField = numberFromTextField!
+
+        // above three lines of code can be simplified to one line
+        let sizeFromTextField = mensShoeSizeTextField.text.toInt()!
+        mensShoeSizeTextField.text = mensShoeSizeTextField.text + " US size"
+        
+//        let conversionConstant = 30
+//        integerFromTextField += conversionConstant
+//        
+//        mensConvertedShoeSizeLabel.hidden = false
+//        let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+//        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+        
+        // simplified
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+    }
 
 }
 
